bc2b49415a0459719500e584fceef1b6b46fd4bd
IBRAHIM7q <alsaadi.aa.ss85@gmail.com>
2025-09-14 06:20:40 +0200

    fifth try

--- app/api/route.ts
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-static'
-export const runtime = 'edge'
-
-export async function GET() {
-  return new Response(JSON.stringify({ status: 'ok' }), {
-    headers: { 'content-type': 'application/json' },
-  })
-}
