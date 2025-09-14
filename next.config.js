ff90c43bc9332b59679c1b59d4623343553ce4a3
IBRAHIM7q <alsaadi.aa.ss85@gmail.com>
2025-09-14 05:52:11 +0200

    fixed

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig
